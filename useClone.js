ff3d04027e003f20c081174810defa478a41620b
Louis <75622872+Kroukrou@users.noreply.github.com>
2022-01-22 12:30:06 +0100

    Create useClone.js

---
+++ useClone.js
@@ -0,0 +1,50 @@
+class Clone{
+
+    // @param array = [] 
+    
+    constructor(img, initialize = 0, array, option){
+
+      
+        this.img = img
+        this.initialize = initialize
+        this.array = array
+        this.option = option
+
+    }
+
+    previousVehicule(){
+
+    this.img.previousElementSibling.addEventListener("click", (e)=>{
+
+        --this.initialize   
+        if(this.initialize < 0){
+    
+          this.initialize += 3
+    
+         }
+         this.img.src = this.array[this.option][this.initialize]
+      
+    
+     })
+    }
+    
+    nextVehicule(){
+     this.img.nextElementSibling.addEventListener("click", (e)=>{
+    
+        ++this.initialize 
+        if(this.initialize === 3){
+    
+        this.initialize -= 3
+    
+       } 
+       this.img.src = this.array[this.option][this.initialize]
+       
+    
+    })
+
+    }
+
+
+
+
+}
