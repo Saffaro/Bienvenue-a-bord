4e39be50bdf043b0e0993c58d82b56606d22332b
Louis <75622872+Kroukrou@users.noreply.github.com>
2022-01-22 12:29:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,31 +49,9 @@ function startSlide(){
 
 startSlide()
 
-
- let peugeot = document.querySelector(".peugeot")
- let previousPeugeot = document.querySelector(".previous__peugeot")  
- let nextPeugeot = document.querySelector(".next__peugeot") 
- let arrayPeugeot = ["img/vehicule1.png","img/peugeot-208-noir.png","img/Peugeot-208.png"]
- let initializeArrayPeugeot = 0
- 
- let ford = document.querySelector(".ford")
- let previousFord = document.querySelector(".previous__ford")  
- let nextFord = document.querySelector(".next__ford")  
- let arrayFord = ["img/vehicule2.png","img/ford-focus-2018-side-view-1.png","img/frontview-ford.png"]
- let initializeArrayFord = 0
- 
- let audi = document.querySelector(".audi")
- let previousAudi = document.querySelector(".previous__audi")  
- let nextAudi = document.querySelector(".next__audi")  
- let arrayAudi = ["img/vehicule3.png","img/index.png","img/MX-audi.png"]
- let initializeArrayAudi = 0
- 
- let opel = document.querySelector(".opel")
- let previousOpel = document.querySelector(".previous__opel")  
- let nextOpel = document.querySelector(".next__opel") 
- let arrayOpel = ["img/vehicule4.png","img/opel18.png","img/mokka-opel.png"]
- let initializeArrayOpel = 0 
- let arrayClone = [
+let arraySection = [document.querySelector("section:first-of-type"),document.querySelector("section:nth-of-type(2)"),document.querySelector("section:nth-of-type(3)"), document.querySelector("section:nth-of-type(4)")]
+let img = [ document.querySelector(".peugeot"),document.querySelector(".ford"),document.querySelector(".audi"),document.querySelector(".opel")]
+let arrayVehicule = [
 
    ["img/vehicule1.png","img/peugeot-208-noir.png","img/Peugeot-208.png"],
    ["img/vehicule2.png","img/ford-focus-2018-side-view-1.png","img/frontview-ford.png"],
@@ -82,7 +60,7 @@ startSlide()
 
 ] 
 let body = document.querySelector("body") 
-let arraySection = [document.querySelector("section:first-of-type"),document.querySelector("section:nth-of-type(2)"),document.querySelector("section:nth-of-type(3)"), document.querySelector("section:nth-of-type(4)")]
+
 
 window.addEventListener("scroll", (e)=>{
 
@@ -95,40 +73,15 @@ window.addEventListener("scroll", (e)=>{
      for(let j = 0; j <= 3; j++){
      
       let add = Math.floor(Math.random() * 4)
-
-      let i = arraySection[add].cloneNode(true)
-      let imgClone = i.firstElementChild.querySelector('img')
-      let previousSvgClone = imgClone.previousElementSibling
-      let nextSvgClone = imgClone.nextElementSibling
-      let initializeClone = 0
+      let clone = arraySection[add].cloneNode(true)
+      let imgClone = clone.firstElementChild.querySelector('img')
      
-      previousSvgClone.addEventListener("click", (e)=>{
-
-         --initializeClone   
-         if(initializeClone < 0){
+       let vehiculeClone = new Clone(imgClone, initialize = 0, arrayVehicule, add)
+       vehiculeClone.nextVehicule()
+       vehiculeClone.previousVehicule()
      
-           initializeClone += 3
-     
-          }
-          imgClone.src = arrayClone[add][initializeClone]
-         console.log("clicked")
-     
-      })
-     
-      nextSvgClone.addEventListener("click", (e)=>{
-     
-         ++initializeClone 
-         if(initializeClone === 3){
-     
-         initializeClone -= 3
-     
-        } 
-        imgClone.src = arrayClone[add][initializeClone]
-        
-     
-     })
       
-      container.appendChild(i)
+      container.appendChild(clone)
       
    } 
 
@@ -142,107 +95,21 @@ window.addEventListener("scroll", (e)=>{
 
 })
 
-previousPeugeot.addEventListener("click", (e)=>{
+let vehicule1 = new Vehicule(img, initialize = 0, arrayVehicule, 0)
+let vehicule2 = new Vehicule(img, initialize = 0, arrayVehicule, 1)
+let vehicule3 = new Vehicule(img, initialize = 0, arrayVehicule, 2)
+let vehicule4 = new Vehicule(img, initialize = 0, arrayVehicule, 3)
 
-    --initializeArrayPeugeot    
-    if(initializeArrayPeugeot < 0){
 
-      initializeArrayPeugeot += 3
+vehicule1.previousVehicule()
+vehicule2.previousVehicule()
+vehicule3.previousVehicule()
+vehicule4.previousVehicule()
 
-     }
-     peugeot.src = arrayPeugeot[initializeArrayPeugeot]
-    
 
- })
+vehicule1.nextVehicule()
+vehicule2.nextVehicule()
+vehicule3.nextVehicule()
+vehicule4.nextVehicule()
 
- nextPeugeot.addEventListener("click", (e)=>{
-
-    ++initializeArrayPeugeot 
-    if(initializeArrayPeugeot === 3){
-
-    initializeArrayPeugeot -= 3
-
-   } 
-   peugeot.src = arrayPeugeot[initializeArrayPeugeot]
-   
-
-})
-
-previousFord.addEventListener("click", (e)=>{
-
-   --initializeArrayFord  
-   if(initializeArrayFord < 0){
-
-    initializeArrayFord += 3
-
-   }  
-   ford.src = arrayFord[initializeArrayFord]
-  
-
-})
-
-nextFord.addEventListener("click", (e)=>{
-
-    ++initializeArrayFord   
-    if(initializeArrayFord === 3){
-
-    initializeArrayFord -= 3
-
-   }   
-    ford.src = arrayFord[initializeArrayFord]
- 
-
-})
-
-previousAudi.addEventListener("click", (e)=>{
-
-   --initializeArrayAudi 
-   if(initializeArrayAudi < 0){
-
-    initializeArrayAudi += 3
-
-   }  
-   audi.src = arrayAudi[initializeArrayAudi]
-  
-
-})
-
-nextAudi.addEventListener("click", (e)=>{
-
-  ++initializeArrayAudi 
-  if(initializeArrayAudi === 3){
-
-  initializeArrayAudi -= 3
-
-  }  
-  audi.src = arrayAudi[initializeArrayAudi]
- 
-
-})
-
-previousOpel.addEventListener("click", (e)=>{
-
-   --initializeArrayOpel 
-   if(initializeArrayOpel < 0){
-
-    initializeArrayOpel += 3
-
-   }  
-   opel.src = arrayOpel[initializeArrayOpel]
-  
-
-})
-
-nextOpel.addEventListener("click", (e)=>{
-
-  ++initializeArrayOpel 
-  if(initializeArrayOpel === 3){
-
-  initializeArrayOpel -= 3
-
-  }  
-  opel.src = arrayOpel[initializeArrayOpel]
- 
-
-})
 }
