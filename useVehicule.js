49e4d39b31053a1fb46113ecea2c4888fe167e95
Louis <75622872+Kroukrou@users.noreply.github.com>
2022-01-22 12:30:43 +0100

    Create useVehicule.js

---
+++ useVehicule.js
@@ -0,0 +1,50 @@
+class Vehicule{
+
+    // @param array = [] 
+    
+    constructor(img, initialize = 0, array, option){
+
+      
+        this.img = img
+        this.initialize = initialize
+        this.array = array
+        this.option = option
+
+    }
+
+    previousVehicule(){
+
+    this.img[this.option].previousElementSibling.addEventListener("click", (e)=>{
+
+        --this.initialize   
+        if(this.initialize < 0){
+    
+          this.initialize += 3
+    
+         }
+         this.img[this.option].src = this.array[this.option][this.initialize]
+      
+    
+     })
+    }
+    
+    nextVehicule(){
+     this.img[this.option].nextElementSibling.addEventListener("click", (e)=>{
+    
+        ++this.initialize 
+        if(this.initialize === 3){
+    
+        this.initialize -= 3
+    
+       } 
+       this.img[this.option].src = this.array[this.option][this.initialize]
+       
+    
+    })
+
+    }
+
+
+
+
+}
